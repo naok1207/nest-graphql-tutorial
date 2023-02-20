08e82a32aaaa70e35a5c23d36aba80cde9bcf722
東直希 <azumanaoki@azumanaokinoMacBook-Pro.local>
2023-02-21 02:40:44 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+**set up**
+
+```
+yarn install
+devbox add postgresql
+devbox run initdb
+> password
+> password
+```
+
+**start**
+
+```
+devbox shell
+npx prisma migrate dev --name post
+npx prisma studio
+yarn run start
+```
+
+`devbox run initdb`を実行すると以下の二つのエラーが出るが無視
+
+```
+Error: Service "postgresql" failed to start
+source: exit status 1
+```
+
+```
+Error: Service "postgresql" failed to stop
+source: exit status 1
+```
---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+import { join } from 'path';
+import { PostsResolver } from './post/posts.resolver';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService, PrismaService, PostsResolver],
+})
+export class AppModule {}
